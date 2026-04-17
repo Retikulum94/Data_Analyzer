aae0e4df094d96c7f989e623f577f36dd98f903c
Retikulum94 <david.brunner94@gmail.com>
2026-04-17 11:28:20 +0200

    erste Anpassungen

--- views/home.py
+++ views/home.py
@@ -2,11 +2,15 @@ import streamlit as st
 
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
-st.title('BMI Rechner')
+st.title('Data Analyzer')
 
-st.markdown("Die Anwendung ermöglicht es Ihnen, Ihren BMI zu berechnen")
+st.markdown("Ein einfaches Tool, um Passing-Bablok Diagramme zu erstellen.")
 
-st.info("""Der BMI ist ein Screening-Tool, aber keine Diagnose für Körperfett oder Gesundheit.
-Bitte konsultieren Sie einen Arzt für eine vollständige Beurteilung.""")
+st.info("""asdf""")
 
-st.write("Diese App wurde von Samuel Wehrli (wehs@zhaw.ch) im Rahmen des Moduls 'BMLD Informatik 2' an der ZHAW entwickelt.")
+st.write("Diese App wurde von" \
+"Dennis Bailer (Baileden@students.zhaw.ch)" \
+- David Brunner (brunndav@students.zhaw.ch)" \
+- Frochaux Noémie (frochnoe@students.zhaw.ch)" \
+- Vagias Dimitrios (vagiadim@students.zhaw.ch)" \
+im Rahmen des Moduls 'BMLD Informatik 2' an der ZHAW entwickelt.")
