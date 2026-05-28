33eb1419a568d5356193685fb48a5601985b1458
Retikulum94 <david.brunner94@gmail.com>
2026-05-28 09:04:08 +0200

    Verlinkungen fix

--- views/home.py
+++ views/home.py
@@ -30,18 +30,22 @@ div.stButton > button {
 </style>
 
 """, unsafe_allow_html=True)
-col1, col2, col3 = st.columns(3)
-
+col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    if st.button("📊 Neue Analyse", use_container_width=True):
+    if st.button("📊 Passing-Bablok", use_container_width=True):
         st.switch_page("views/passing_bablok2.py")
 
 with col2:
-    st.button("📁 Verlauf", use_container_width=True)
+    if st.button("📊 Bland-Altman", use_container_width=True):
+        st.switch_page("views/bland_altman2.py")
 
 with col3:
-    st.button("❓ Hilfe", use_container_width=True)
+    st.button("📁 Verlauf", use_container_width=True)
+
+with col4:
+    if st.button("❓ Hilfe", use_container_width=True):
+        st.switch_page("views/Benutzeranleitung_1.py")
 
 st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("---")
